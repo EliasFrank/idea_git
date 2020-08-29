907ce6d489cef7975757f27ce6cf195adcf2f76f
elias <elias_git@qq.com>
2020-08-29 08:25:41 +0800

    branch hl create

--- src/main/java/com/jxau/Hello.java
+++ src/main/java/com/jxau/Hello.java
@@ -3,6 +3,7 @@ package com.jxau;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello hl");
     }
     public void addMethod(){}
 
