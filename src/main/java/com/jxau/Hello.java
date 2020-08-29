fb06089beceb8918b980e82c7e5e893834426a16
elias <elias_git@qq.com>
2020-08-29 09:08:59 +0800

    test ssh

--- src/main/java/com/jxau/Hello.java
+++ src/main/java/com/jxau/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
         System.out.println("master");
         System.out.println("hello hl");
         System.out.println("hello hl");
+        System.out.println("test ssh");
     }
     public void addMethod(){}
 
